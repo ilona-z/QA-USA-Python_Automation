4253058d57e5f1ae62a9ad5523ba8d4ad9fe6fb3
ilonazhalovska <ilonayusenko@gmail.com>
2025-02-12 22:08:01 -0500

    first commit

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
 # File with constants
+# URL for Urban Routes (initially empty)
+URBAN_ROUTES_URL = 'https://cnt-140f9d7f-915a-4dfd-8f27-44d982dc0b55.containerhub.tripleten-services.com'
+
+# Test data constants
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
